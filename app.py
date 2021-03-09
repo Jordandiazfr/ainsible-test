de2fbedc0b05a49769ed424ac03348320c121496
Jordandiazfr <jordandiazfr@gmail.com>
2021-03-09 16:50:34 +0100

    wsgi requirement

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return 'hello'
+
 
 @app.route("/health")
 def health():
     return jsonify({'status': 'ok'})
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True, port=5000)
