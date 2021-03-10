ea307431177feb76fef4ffce247eafa18ec3466b
Jordandiazfr <jordandiazfr@gmail.com>
2021-03-10 12:22:09 +0100

    venv

--- app.py
+++ app.py
@@ -12,5 +12,6 @@ def health():
     return jsonify({'status': 'ok'})
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5000)
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0', debug=True, port=5000)
+#
