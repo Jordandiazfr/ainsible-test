1fb67f5ffc42e484ad3d7a2e049e3716068fbcbb
Jordandiazfr <jordandiazfr@gmail.com>
2021-03-11 10:14:03 +0100

    templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import os
 from dbserver import PostGreSQL
 import random
@@ -14,7 +14,7 @@ data = "Test" + str(r)
 
 @app.route("/")
 def index():
-    return "hello world"
+    return render_template("index.html")
 
 
 @app.route("/db")
